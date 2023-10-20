0c710019e1af77ae75dd0bd95904c9f0e5b01161
Ramonrpro <122824092+Ramonrpro@users.noreply.github.com>
2023-10-20 07:24:55 -0300

    commit inicial + contador click

---
+++ scripts/game.js
@@ -0,0 +1,136 @@
+const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
+const clickes = document.querySelector('.click');
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+let contador = 1;
+document.body.addEventListener('click', () => {
+    clickes.textContent = contador
+    contador++
+
+})
+
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length === 20) {
+        clearInterval(this.loop);
+        alert(`Parabéns ${spanPlayer.textContent}, seu tempo foi: ${timer.textContent} segundos!`);
+    }
+}
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if (firstCharacter === secondCharacter) {
+
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
+            firstCard = '';
+            secondCard = '';
+
+            checkEndGame();
+
+    } else {
+
+        setTimeout(() => {
+            
+            firstCard.classList.remove('reveal-card')
+            secondCard.classList.remove('reveal-card')
+
+            firstCard = '';
+            secondCard = '';
+
+        }, 500)
+    }
+}
+
+const revealCard = ({ target}) => {
+
+    if (target.parentNode.className.includes('reveal-card')) {
+        return;
+    }
+
+    if (firstCard === '') {
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+    }else if (secondCard === '') {
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+
+}
+
+const createCard = (character) => {
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../img/${character}.png')`
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', revealCard)
+    card.setAttribute('data-character', character)
+
+    return card;
+}
+
+const loadGame = () => {
+
+    const duplicatecharacters = [ ...characters, ...characters ];
+
+    const shuffledArray = duplicatecharacters.sort(() => Math.random() - 0.5);
+
+    
+
+    shuffledArray.forEach((character) => {
+
+        const card = createCard(character);
+        grid.appendChild(card);
+
+    });
+}
+
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+        const currentTime =+ timer.textContent;
+        timer.textContent = currentTime + 1;
+    }, 1000)
+}
+
+window.onload = () => {
+
+    spanPlayer.textContent = localStorage.getItem('player');
+    startTimer();
+    loadGame();
+}
+
